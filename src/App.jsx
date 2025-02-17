2d017956a9f62176ce0353eab87c250c067a69f4
wdwf <w_dwf@hotmail.com>
2025-02-17 18:19:51 -0300

    :construction: feat: implementa lista de posts com formatação de data e conteúdo dinâmico

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post.jsx"
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/expo-starter.png',
+      name: 'Cyber Dev',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera, beleza?' },
+      { type: 'paragraph', content: 'Estou fazendo um post para testar o componente de postagem.' },
+      { type: 'link', content: 'post.dev/feedignite' }
+    ],
+    publishedAt: new Date('2025-02-17 17:23:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/shadcn.png',
+      name: 'Bob Bobistico',
+      role: 'Fullstack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera, beleza?' },
+      { type: 'paragraph', content: 'Estou fazendo um post para testar o componente de postagem.' },
+      { type: 'link', content: 'post.dev/feedignite' }
+    ],
+    publishedAt: new Date('2025-02-15 20:03:00')
+  }
+]
+
 function App() {
   return (
     <>
@@ -11,10 +42,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Bob Fernandes"
-            content="Texto..."
-          />
+          {
+            posts.map(post => (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ))
+          }
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,48 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post(props) {
+export function Post({ author, content, publishedAt }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/expo-starter.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Cyber Dev</strong>
-                        <span>Fullstack Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="11 de Maio ás 08:13h" dateTime="2024-05-11 08:13:38">Publicado a 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeNow}
+                </time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
+                {
 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p><a href="">👉 cyber.design/doctorcare</a></p>
+                    content.map((line, index) => {
+                        switch (line.type) {
+                            case 'paragraph':
+                                return <p key={index}>{line.content}</p>
+                            case 'link':
+                                return <a key={index} href={line.content}>{line.content}</a>
+                            default:
+                                return null
+                        }
+                    })
+                }
 
                 <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#rocketseat</a></p>
             </div>
