08be3a97292de2205e8ef0b69177b19d1669f87e
wdwf <w_dwf@hotmail.com>
2025-02-18 15:57:25 -0300

    :construction: feat: add funcionalidade de like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react'
 
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment() {
+        setLikeCount(state => state + 1);
+    }
+
     function handleDeleteComment(content) {
         onDeleteComment(content);
     }
@@ -25,9 +32,9 @@ export function Comment({ content, onDeleteComment }) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment} title='Aplaudir comentário'>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
